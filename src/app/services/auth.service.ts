40392389dfccbe94a289b907f4e2d743b1eebd64
Mario <mario.lopez.batres@gmail.com>
2019-06-18 09:17:22 +0200

    Timeout to logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
+  private tokenTimer: any;
 
   constructor(private httpClient: HttpClient, private router: Router) {}
 
@@ -40,13 +41,25 @@ export class AuthService {
     const authData: AuthData = { email: email, password: password };
 
     // Response in user.ts is res.status(200)... token
+    // ExpiresIn comes from user.js
     this.httpClient
-      .post<{ token: string }>("http://localhost:3000/api/user/login", authData)
+      .post<{ token: string; expiresIn: number }>(
+        "http://localhost:3000/api/user/login",
+        authData
+      )
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         // Content could be null even with the possitive subscribe
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(
+            () => {
+              this.logout();
+            },
+            // it works in milliseconds and not in secconds
+            expiresInDuration * 1000
+          );
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(["/"]);
@@ -59,6 +72,7 @@ export class AuthService {
     this.isAuthenticated = false;
     // Push the new value to the listeners
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(["/"]);
   }
 }
